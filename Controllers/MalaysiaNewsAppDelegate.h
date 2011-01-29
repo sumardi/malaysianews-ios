2b9f5f1a113e1974618b121913284f5a4d688a4e
smd <me@sumardi.net>
2011-01-30 04:10:54 +0800

    Removed core data

--- Controllers/MalaysiaNewsAppDelegate.h
+++ Controllers/MalaysiaNewsAppDelegate.h
@@ -20,28 +20,14 @@
 //	along with Malaysia News. If not, see <http://www.gnu.org/licenses/>
 
 #import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
 
 @interface MalaysiaNewsAppDelegate : NSObject <UIApplicationDelegate> {
-    
     UIWindow *window;
 	UINavigationController *navigationController;
-    
-@private
-    NSManagedObjectContext *managedObjectContext_;
-    NSManagedObjectModel *managedObjectModel_;
-    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
-
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (NSURL *)applicationDocumentsDirectory;
-- (void)saveContext;
 
 @end
 
