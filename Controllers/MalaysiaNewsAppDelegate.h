691c24ae1761be24fa261594ce28845654a9053c
smd <me@sumardi.net>
2011-01-30 01:29:44 +0800

    Initial imports.

---
+++ Controllers/MalaysiaNewsAppDelegate.h
@@ -0,0 +1,45 @@
+//
+//  MalaysiaNewsAppDelegate.h
+//  MalaysiaNews
+//
+//  Created by Sumardi Shukor on 1/30/11.
+//  Copyright 2011 Software Machine Development. All rights reserved.
+//	<support[at]smd.com.my>
+//
+//	Malaysia News is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+// 
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+// 
+//	You should have received a copy of the GNU General Public License
+//	along with Malaysia News. If not, see <http://www.gnu.org/licenses/>
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MalaysiaNewsAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
